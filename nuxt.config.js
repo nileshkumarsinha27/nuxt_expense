57ca04241f0c22f7294c2139306edf7e4c3b387c
nileshkumarsinha27 <nileshsinha.sinha0@gmail.com>
2021-07-06 12:18:10 +0530

    Add files via upload

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'expense',
+    target: 'static',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
